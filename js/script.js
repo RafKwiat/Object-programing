e7668186250d6c257560abddcd38f0a9dd351578
Rafal Kwiatkowski <rafalkwiatkowski8@gmail.com>
2019-07-06 14:50:22 +0200

    Hello button

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
-function phone(brand, price, color, camera) {
-    this.brand = brand;
-    this.price = price;
-    this.color = color;
-    this.camera = camera;
+function button(text) {
+    this.text = text || 'Hello';
 }
 
-phone.prototype.printInfo = function () {
-    console.log("The phone brand is " + this.brand + ", it's color is " + this.color + " and the price is " + this.price + " zl. It's camera has " + this.camera + " Mpx.")
-}
+button.prototype = {
+    create: function() {
+        var self = this;
+        this.element = document.createElement('button');
+        this.element.innerText = this.text;
+        this.element.addEventListener('click', function() {
+            alert(self.text)
+        });
+        document.body.appendChild(this.element);
+    }
+} 
 
-var SamsungGalaxyS6 = new phone("Samsung", 2300, "silver", 16);
-var iPhone6s = new phone("Apple", 2500, "space grey", 12);
-var OnePlusOne = new phone("Oneplus", 1000, "black", 13);
-
-SamsungGalaxyS6.printInfo();
-iPhone6s.printInfo();
-OnePlusOne.printInfo();
+var btn1 = new button ('Hello');
+btn1.create();
+    
